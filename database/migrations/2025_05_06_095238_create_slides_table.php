9e704690a47f38ea36824455252fdb043e523d6d
Mrinmoy Barman <mrinmoy.barman@accf.in>
2025-05-06 16:47:01 +0530

    Slides half done & video is pending

---
+++ database/migrations/2025_05_06_095238_create_slides_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hospitalId');
+            $table->integer('addedBy');
+            $table->string('photo');
+            $table->integer('indexx');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
